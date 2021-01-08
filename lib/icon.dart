0cbaff36d28928ab17910f260a384a94e3301846
Usamaamjad56 <usamaamjad56@gmail.com>
2021-01-08 20:10:37 +0500

    add icon

---
+++ lib/icon.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class iPhoneXXS11Pro1 extends StatelessWidget {
+  iPhoneXXS11Pro1({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff3d3d3d),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(98.0, 316.0),
+            child:
+                // Adobe XD layer: 'logo' (group)
+                SizedBox(
+              width: 179.0,
+              height: 179.0,
+              child: Stack(
+                children: <Widget>[
+                  Pinned.fromSize(
+                    bounds: Rect.fromLTWH(0.0, 0.0, 179.1, 179.1),
+                    size: Size(179.1, 179.1),
+                    pinLeft: true,
+                    pinRight: true,
+                    pinTop: true,
+                    pinBottom: true,
+                    child:
+                        // Adobe XD layer: 'path' (shape)
+                        SvgPicture.string(
+                      _svg_l7wxkt,
+                      allowDrawingOutsideViewBox: true,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Pinned.fromSize(
+                    bounds: Rect.fromLTWH(89.5, 0.0, 89.5, 89.5),
+                    size: Size(179.1, 179.1),
+                    pinRight: true,
+                    pinTop: true,
+                    fixedWidth: true,
+                    fixedHeight: true,
+                    child:
+                        // Adobe XD layer: 'path' (shape)
+                        SvgPicture.string(
+                      _svg_4s38s7,
+                      allowDrawingOutsideViewBox: true,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_l7wxkt =
+    '<svg viewBox="1434.3 1088.0 179.1 179.1" ><defs><linearGradient id="gradient" x1="0.5" y1="0.0" x2="0.5" y2="1.0"><stop offset="0.0" stop-color="#ffb45ab3"  /><stop offset="1.0" stop-color="#ff5a2d5a"  /></linearGradient></defs><path  d="M 1578.423828125 1177.529418945313 C 1578.423828125 1207.618530273438 1553.944580078125 1232.09912109375 1523.855346679688 1232.09912109375 C 1499.881958007813 1232.09912109375 1479.484130859375 1216.552001953125 1472.1767578125 1195.016357421875 L 1523.855346679688 1195.016357421875 L 1523.855346679688 1160.04248046875 L 1472.1767578125 1160.04248046875 C 1479.485229492188 1138.507934570313 1499.881958007813 1122.9609375 1523.855346679688 1122.9609375 L 1523.855346679688 1087.987060546875 C 1474.481689453125 1087.987060546875 1434.31298828125 1128.155639648438 1434.31298828125 1177.529418945313 C 1434.31298828125 1226.903198242188 1474.481689453125 1267.072998046875 1523.855346679688 1267.072998046875 C 1573.229125976563 1267.072998046875 1613.397705078125 1226.903198242188 1613.397705078125 1177.529418945313 L 1578.423828125 1177.529418945313 Z" fill="url(#gradient)" stroke="#776d6d" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_4s38s7 =
+    '<svg viewBox="1523.9 1088.0 89.5 89.5" ><defs><linearGradient id="gradient" x1="0.5" y1="0.0" x2="0.5" y2="1.0"><stop offset="0.0" stop-color="#ffb45ab3"  /><stop offset="1.0" stop-color="#ff5a2d5a"  /></linearGradient></defs><path transform="translate(12.73, 0.0)" d="M 1600.663330078125 1177.529418945313 L 1565.689453125 1177.529418945313 C 1565.689453125 1147.440185546875 1541.210205078125 1122.9609375 1511.120971679688 1122.9609375 L 1511.120971679688 1087.987060546875 C 1560.494750976563 1087.987060546875 1600.663330078125 1128.155639648438 1600.663330078125 1177.529418945313 Z" fill="url(#gradient)" fill-opacity="0.5" stroke="#776d6d" stroke-width="1" stroke-opacity="0.5" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
