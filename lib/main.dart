4a0b8c461545b53ce8d4ca36776f1366fd3fb9ec
Usamaamjad56 <usamaamjad56@gmail.com>
2021-01-08 20:12:02 +0500

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 10,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text('Usama.  ' 'FA17-BSE-049'),
+        image: new Image.asset('image.jpeg'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.grey);
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'final project app',
+      theme: ThemeData(primarySwatch: Colors.red),
+    );
+  }
+}
